56fb5ff7f859ca16b4388a1051fc7d27e91ee5c6
imasshaWe <imasha98.we@gmail.com>
2023-10-21 22:01:42 +0530

    Added app launcher provider

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { RoutesConfig } from '../config';
+import AppLauncherProvider from './providers/AppLauncherProvider';
 
 export function App() {
   const router = createBrowserRouter(RoutesConfig.routes);
 
-  return <RouterProvider router={router} />;
+  return (
+    <AppLauncherProvider>
+      <RouterProvider router={router} />
+    </AppLauncherProvider>
+  );
 }
 
 export default App;
