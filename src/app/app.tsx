eecbb6adc8d9df8c0ecef3f7e85a004cca123105
imasshaWe <imasha98.we@gmail.com>
2023-11-21 21:53:45 +0530

    Added: token auth

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,29 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { RoutesConfig } from '../config';
 import AppLauncherProvider from './providers/AppLauncherProvider';
-import { UserAuthProvider } from '@user-auth';
+import { AuthData, UserAuthProvider } from '@user-auth';
+import { httpClient } from '@http-client';
 
 export function App() {
   const router = createBrowserRouter(RoutesConfig.routes);
 
+  const handleAuthStateChange = (authData: AuthData | null) => {
+    if (authData?.token) {
+      httpClient.interceptors.request.use((config) => {
+        console.log(config.url?.includes('auth'));
+        if (!config.url?.includes('auth')) {
+          config.headers['Authorization'] = `Bearer ${authData.token}`;
+        }
+        return config;
+      });
+    }
+  };
+
   return (
     <AppLauncherProvider>
-      <UserAuthProvider>
+      <UserAuthProvider
+        onAuthStateChange={(authData) => handleAuthStateChange(authData)}
+      >
         <RouterProvider router={router} />
       </UserAuthProvider>
     </AppLauncherProvider>
