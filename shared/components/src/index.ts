6fb1d5ab4eb4a0b7a792f203efd7af577bc5635f
imasshaWe <imasha98.we@gmail.com>
2024-01-18 22:15:52 +0530

    feat:modal

--- shared/components/src/index.ts
+++ shared/components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/button';
 export * from './lib/input';
+export * from './lib/modal';
 export * from './lib/spinner';
--- shared/components/src/lib/modal/index.ts
+++ shared/components/src/lib/modal/index.ts
@@ -1,2 +1,1 @@
 export { Modal, ModalHandler } from './modal';
-export { ModalProvider, useModal } from './ModalProvider';
