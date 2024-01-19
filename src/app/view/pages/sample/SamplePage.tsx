d357c67ddc7bb288136c03d1c7ea0c0b1fa41f63
imasshaWe <imasha98.we@gmail.com>
2024-01-19 21:41:30 +0530

    Add new design

--- src/app/view/pages/sample/SamplePage.tsx
+++ src/app/view/pages/sample/SamplePage.tsx
@@ -1,17 +1,51 @@
-import { useRequest } from '@http-client';
-import { FC, useEffect } from 'react';
+import { Button, Modal, ModalHandler, TextInput, useAlert } from '@components';
+import { FC, useRef } from 'react';
 
 const SamplePage: FC = () => {
-  const { get } = useRequest();
+  const modalRef = useRef<ModalHandler>(null);
+  const { showAlert } = useAlert();
 
-  useEffect(() => {
-    get('company').then((res) => {
-      console.log(res);
+  const askToDelete = () => {
+    showAlert({
+      title: 'Are you sure?',
+      message: 'This action cannot be undone.',
+      type: 'confirmation',
+      color: 'warning',
+      handleConfirm: () => {
+        console.log('confirmed');
+      },
     });
-  }, []);
+  };
+
+  const openModal = () => {
+    modalRef.current?.open();
+  };
+
   return (
-    <div>
-      <h1>Sample Page</h1>
+    <div className="flex flex-col items-start gap-2">
+      <Button onClick={() => openModal()}>Create</Button>
+      <Button color="warning" onClick={() => askToDelete()}>
+        Delete
+      </Button>
+
+      <Modal ref={modalRef} title="Create Profile">
+        <div className="flex flex-col gap-4 ">
+          <TextInput label="First Name" />
+          <TextInput label="Last Name" />
+          <TextInput label="Email" />
+
+          <div className="flex justify-end gap-2">
+            <Button
+              onClick={() => modalRef.current?.close()}
+              variant="text"
+              color="secondary"
+            >
+              Cancel
+            </Button>
+            <Button color="success">Save</Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
