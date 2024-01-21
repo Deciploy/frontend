d4ba87de4ab434ba75cfdd65aeacc8abcc79b056
imasshaWe <imasha98.we@gmail.com>
2024-01-21 21:11:13 +0530

    Updated

--- shared/components/src/lib/input/PasswordInput.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { BaseInput, InputProps } from './BaseInput';
-
-type PasswordInputProps = Omit<InputProps, 'type'>;
-
-export const PasswordInput: FC<PasswordInputProps> = (props) => {
-  return <BaseInput {...props} type="password" />;
-};
