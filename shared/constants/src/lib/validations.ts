f3ae57e695c9d84c054589a66cf357fe435f4032
NethminiDevyanjalee <nethminid1999@gmail.com>
2024-02-15 23:00:54 +0530

    Implemented user screen

--- shared/constants/src/lib/validations.ts
+++ shared/constants/src/lib/validations.ts
@@ -8,3 +8,10 @@ export const LoginSchema = Yup.object().shape({
 export const TeamSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
 });
+
+export const UserSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email').required('The email is required'),
+  role: Yup.string().required('role is required'),
+  team: Yup.string().required('team is required'),
+});
