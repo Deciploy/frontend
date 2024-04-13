95822816d80f3cd9daf8d5dbd6deb5dba3e80036
imasshaWe <imasha98.we@gmail.com>
2024-04-13 15:18:07 +0530

    Add react-toastify package for displaying toast notifications and import its CSS file

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import { AlertProvider } from '@components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import { RoutesConfig } from '../config';
 import { AppLauncherProvider, AuthProvider } from './providers';
@@ -10,24 +11,13 @@ const queryClient = new QueryClient();
 export function App() {
   const router = createBrowserRouter(RoutesConfig.routes);
 
-  // const handleAuthStateChange = (state: AuthState | null) => {
-  //   console.log('Auth state changed', state);
-  //   if (state?.token) {
-  //     httpClient.interceptors.request.use((config) => {
-  //       if (!config.url?.includes('auth')) {
-  //         config.headers['Authorization'] = `Bearer ${state.token}`;
-  //       }
-  //       return config;
-  //     });
-  //   }
-  // };
-
   return (
     <AppLauncherProvider>
       <AuthProvider>
         <QueryClientProvider client={queryClient}>
           <AlertProvider>
             <RouterProvider router={router} />
+            <ToastContainer />
           </AlertProvider>
         </QueryClientProvider>
       </AuthProvider>
