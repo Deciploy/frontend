f0993640b3cff5e59d33de78c0dcb93a2e4b75a5
imasshaWe <imasha98.we@gmail.com>
2024-05-30 23:16:39 +0530

    refactor: Remove SamplePage component and update app-routes

--- src/config/routes/app-routes.tsx
+++ src/config/routes/app-routes.tsx
@@ -1,25 +1,21 @@
-import { AppRoute } from './interface';
-
-import LoginPage from '../../app/view/pages/auth/LoginPage';
-import HomePage from '../../app/view/pages/home/HomePage';
-import SamplePage from '../../app/view/pages/sample/SamplePage';
-import TeamPage from '../../app/view/pages/team/TeamPage';
-import UserPage from '../../app/view/pages/user/UserPage';
-
-import { MdDashboard } from 'react-icons/md';
 import { AiFillEye } from 'react-icons/ai';
 import { FaUsers } from 'react-icons/fa';
-import { FaUser } from "react-icons/fa";
+import { FaUser } from 'react-icons/fa';
+import { MdDashboard } from 'react-icons/md';
 
 import { ProtectedRoute } from '../../app/view/common/ProtectedRoute';
-
+import LoginPage from '../../app/view/pages/auth/LoginPage';
+import HomePage from '../../app/view/pages/home/HomePage';
+import TeamPage from '../../app/view/pages/team/TeamPage';
+import UserPage from '../../app/view/pages/user/UserPage';
+import { AppRoute } from './interface';
 
 export const routeNames = {
   home: '/',
   login: '/login',
   sample: '/sample',
   team: '/team',
-  user: '/user'
+  user: '/user',
 };
 
 export const appRoutes: AppRoute = {
@@ -59,16 +55,6 @@ export const appRoutes: AppRoute = {
       ),
       title: 'User',
       Icon: FaUser,
-    },
-    {
-      path: routeNames.sample,
-      Component: () => (
-        <ProtectedRoute>
-          <SamplePage />
-        </ProtectedRoute>
-      ),
-      title: 'Sample',
-      Icon: AiFillEye,
     },
   ],
 };
