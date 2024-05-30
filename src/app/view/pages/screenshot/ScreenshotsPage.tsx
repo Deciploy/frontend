921419a851597151f10828653d912729f80123c5
imasshaWe <imasha98.we@gmail.com>
2024-05-31 01:49:42 +0530

    feat: Add team and user selectors to the screenshot page

--- src/app/view/pages/screenshot/ScreenshotsPage.tsx
+++ src/app/view/pages/screenshot/ScreenshotsPage.tsx
@@ -1,81 +1,89 @@
-import { Button, SelectInput } from '@components';
-import { AiOutlineClockCircle } from 'react-icons/ai';
+import { useScreenshotFetch } from '@api';
+import { Button, DateTimeInput, SelectInput, TextInput } from '@components';
+import { Screenshot } from '@types';
+import { useEffect, useState } from 'react';
+import Moment from 'react-moment';
+
+import { TeamSelector, UserSelector } from '../../common';
+import ScreenshotItem from './components/ScreenshotItem';
 
 export default function ScreenshotPage() {
-  const n = 3; // Assume n is the number of timeline entries
+  const [teamId, setTeamId] = useState<string | undefined>(undefined);
+  const [userId, setUserId] = useState<string | undefined>(undefined);
+  const [selectedScreenshot, setSelectedScreenshot] =
+    useState<Screenshot | null>(null);
+
+  const { isLoading, data, refetch } = useScreenshotFetch(
+    '0e7792fd-3f0e-42bb-a1f9-b956bfc1419c'
+  );
+
+  useEffect(() => {
+    if (data?.data?.length) {
+      setSelectedScreenshot(data.data[0]);
+    }
+  }, [data]);
 
   return (
-    <div>
-      <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl p-3">
-        Screenshot
-      </h1>
+    <div className="space-y-4">
+      <h1 className="text-2xl">Screenshot</h1>
 
-      <div className="flex flex-col p-2">
-        <div className="flex space-x-8 p-3">
-          <div>
-            <h1 className="font-bold">Team</h1>
-            <SelectInput
-              options={[
-                { label: 'Option 1', value: '1' },
-                { label: 'Option 2', value: '2' },
-              ]}
-            />
+      <div className="flex gap-3 w-3/4">
+        <TeamSelector
+          className="w-1/4"
+          placeholder="Team"
+          onChange={setTeamId}
+        />
+
+        <UserSelector
+          className="w-1/4"
+          placeholder="User"
+          onChange={setUserId}
+        />
+
+        <DateTimeInput
+          className="w-1/4"
+          placeholder="From"
+          onChange={(value) => console.log(value)}
+        />
+
+        <DateTimeInput
+          className="w-1/4"
+          placeholder="From"
+          onChange={(value) => console.log(value)}
+        />
+      </div>
+
+      <div className="flex">
+        <div className="w-full md:w-3/5 p-4 ">
+          <div className="text-xl  mb-4">
+            Captured At:{' '}
+            <Moment format="dddd, D MMMM YYYY, MMM hh:mm:ss a">
+              {selectedScreenshot?.capturedAt}
+            </Moment>
           </div>
-          <div>
-            <h1 className="font-bold">User</h1>
-            <SelectInput placeholder="Development" fullWidth options={[]} />
+
+          <div className="p-6 ">
+            <img src={selectedScreenshot?.url} className="mb-4 max-w-lg" />
           </div>
-          <div>
-            <h1 className="font-bold">From</h1>
-            <SelectInput placeholder="Development" fullWidth options={[]} />
-          </div>
-          <div>
-            <h1 className="font-bold">To</h1>
-            <SelectInput placeholder="Development" fullWidth options={[]} />
+
+          <div className="p-4 flex space-x-4">
+            <Button>Capture Now</Button>
+            <Button variant="filled" color="warning">
+              Delete
+            </Button>
           </div>
         </div>
 
-        <div className="flex flex-col md:flex-row">
-          <div className="w-full md:w-3/5 p-4 ">
-            <h1 className="text-2xl font-bold mb-4">Captured At:</h1>
-
-            <div className="p-6 ">
-              <img
-                src={
-                  'https://webhost365.net/wp-content/uploads/2021/09/how-to-take-Screenshot.jpg'
-                }
-                className="max-w-full mb-4 max-w-lg"
-              />
-            </div>
-
-            <div className="p-4 flex space-x-4">
-              <Button>Capture Now</Button>
-              <Button variant="filled" color="warning">
-                Delete
-              </Button>
-            </div>
-          </div>
-
-          <div className="w-full md:w-2/5 p-4 ">
-            <h1 className="text-2xl font-bold mb-4">Timeline </h1>
-
-            {Array.from({ length: n }, (_) => (
-              <div>
-                <div className="flex items-center p-3">
-                  <AiOutlineClockCircle size={30} className="mr-2" />
-                  <h3>{`x minutes ago`}</h3>
-                </div>
-                <div className="p-10 border-l border-gray-500 ml-6">
-                  <img
-                    src={
-                      'https://webhost365.net/wp-content/uploads/2021/09/how-to-take-Screenshot.jpg'
-                    }
-                    className="max-w-full mb-4 max-w-xs"
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
+        <div className="w-2/5 max-h-[70vh] overflow-auto">
+          <div className="text-xl mb-4">Timeline</div>
+          {data?.data?.map((screenshot) => (
+            <ScreenshotItem
+              key={screenshot.id}
+              screenshot={screenshot}
+              selected={screenshot.id === selectedScreenshot?.id}
+              onSelect={(screenshot) => setSelectedScreenshot(screenshot)}
+            />
+          ))}
         </div>
       </div>
     </div>
