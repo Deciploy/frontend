eb88df8be433754bfe9a3861e7e4bf6dc5cfd7d4
imasshaWe <imasha98.we@gmail.com>
2024-05-30 20:16:02 +0530

    feat: Add UserRequest interface for user creation

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,13 @@
 import { Company } from "./company";
 import { Team } from "./team";
 
+export interface UserRequest {
+  fullName: string;
+  email: string;
+  roles: string[];
+  teamId?: string;
+}
+
 export interface User {
   id: string;
   fullName: string;
