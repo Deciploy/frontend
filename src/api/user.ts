d3c9929c2c3dcb983024b3f7476a5fde9afe3436
imasshaWe <imasha98.we@gmail.com>
2024-05-30 23:15:43 +0530

    refactor: Update useUserDelete to use DELETE instead of POST for deleting users

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,5 @@
 import { NetworkResponse, User, UserRequest } from "@types";
-import { useFetch, usePatch, usePost } from "src/app/utils/hooks";
+import { useDelete, useFetch, usePatch, usePost } from "src/app/utils/hooks";
 
 export const useUserFetch = () => useFetch<NetworkResponse<User[]>>('user');
 
@@ -7,4 +7,4 @@ export const useUserCreate = () => usePost<NetworkResponse, UserRequest>('user')
 
 export const useUserUpdate = (id: string) => usePatch<NetworkResponse, UserRequest>(`user/${id}`);
 
-export const useUserDelete = (id: string) => usePost<NetworkResponse>(`user/${id}`);
+export const useUserDelete = (id: string) => useDelete<NetworkResponse>(`user/${id}`);
