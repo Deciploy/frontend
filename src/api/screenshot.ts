e2a009cfa82d884809e441cf937b286da85b51bc
imasshaWe <imasha98.we@gmail.com>
2024-06-08 00:24:26 +0530

    refactor: Update useScreenshotFetch to accept undefined url

--- src/api/screenshot.ts
+++ src/api/screenshot.ts
@@ -1,4 +1,4 @@
 import { NetworkResponse, Screenshot } from "@types";
 import { useFetch } from "src/app/utils/hooks";
 
-export const useScreenshotFetch = (userId: string | undefined) => useFetch<NetworkResponse<Array<Screenshot>>>(`screenshot/${userId}`);
+export const useScreenshotFetch = (url: string | undefined) => useFetch<NetworkResponse<Array<Screenshot>>>(`screenshot/${url}`);
