b2998ca4052265fe1b6e182d72c90c336e7500f5
imasshaWe <imasha98.we@gmail.com>
2024-06-07 21:42:31 +0530

    refactor: Update useScreenshotFetch to accept undefined userId

--- src/api/screenshot.ts
+++ src/api/screenshot.ts
@@ -1,4 +1,4 @@
 import { NetworkResponse, Screenshot } from "@types";
 import { useFetch } from "src/app/utils/hooks";
 
-export const useScreenshotFetch = (userId: string) => useFetch<NetworkResponse<Array<Screenshot>>>(`screenshot/${userId}`);
+export const useScreenshotFetch = (userId: string | undefined) => useFetch<NetworkResponse<Array<Screenshot>>>(`screenshot/${userId}`);
