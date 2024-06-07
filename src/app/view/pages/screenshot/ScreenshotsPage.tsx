e59f8f484cf656c4d1575c8cdfeb4448eea1a591
imasshaWe <imasha98.we@gmail.com>
2024-06-07 21:44:53 +0530

    feat: Add lazy loading for images on ScreenshotsPage

--- src/app/view/pages/screenshot/ScreenshotsPage.tsx
+++ src/app/view/pages/screenshot/ScreenshotsPage.tsx
@@ -1,7 +1,14 @@
 import { useScreenshotFetch } from '@api';
-import { Button, DateTimeInput, SelectInput, TextInput } from '@components';
+import {
+  Button,
+  CircleSpinner,
+  DateTimeInput,
+  SelectInput,
+  TextInput,
+} from '@components';
 import { Screenshot } from '@types';
 import { useEffect, useState } from 'react';
+import { RiErrorWarningLine } from 'react-icons/ri';
 import Moment from 'react-moment';
 
 import { TeamSelector, UserSelector } from '../../common';
@@ -13,15 +20,13 @@ export default function ScreenshotPage() {
   const [selectedScreenshot, setSelectedScreenshot] =
     useState<Screenshot | null>(null);
 
-  const { isLoading, data, refetch } = useScreenshotFetch(
-    '0e7792fd-3f0e-42bb-a1f9-b956bfc1419c'
-  );
+  const { isLoading, data: response, refetch } = useScreenshotFetch(userId);
 
   useEffect(() => {
-    if (data?.data?.length) {
-      setSelectedScreenshot(data.data[0]);
+    if (response?.data?.length) {
+      setSelectedScreenshot(response.data[0]);
     }
-  }, [data]);
+  }, [response]);
 
   return (
     <div className="space-y-4">
@@ -52,40 +57,55 @@ export default function ScreenshotPage() {
           onChange={(value) => console.log(value)}
         />
       </div>
-
-      <div className="flex">
-        <div className="w-full md:w-3/5 p-4 ">
-          <div className="text-xl  mb-4">
-            Captured At:{' '}
-            <Moment format="dddd, D MMMM YYYY, MMM hh:mm:ss a">
-              {selectedScreenshot?.capturedAt}
-            </Moment>
-          </div>
-
-          <div className="p-6 ">
-            <img src={selectedScreenshot?.url} className="mb-4 max-w-lg" />
-          </div>
-
-          <div className="p-4 flex space-x-4">
-            <Button>Capture Now</Button>
-            <Button variant="filled" color="warning">
-              Delete
-            </Button>
-          </div>
+      {isLoading && (
+        <div className="flex grow flex-col items-center justify-center mt-48 h-[50vh]">
+          <CircleSpinner size={48} circleClassName="text-primary" />
         </div>
+      )}
 
-        <div className="w-2/5 max-h-[70vh] overflow-auto">
-          <div className="text-xl mb-4">Timeline</div>
-          {data?.data?.map((screenshot) => (
-            <ScreenshotItem
-              key={screenshot.id}
-              screenshot={screenshot}
-              selected={screenshot.id === selectedScreenshot?.id}
-              onSelect={(screenshot) => setSelectedScreenshot(screenshot)}
-            />
-          ))}
-        </div>
-      </div>
+      {!isLoading &&
+        (response?.data?.length !== 0 ? (
+          <div className="flex">
+            <div className="w-full md:w-3/5 p-4 ">
+              <div className="text-xl  mb-4">
+                Captured At:{' '}
+                <Moment format="dddd, D MMMM YYYY, MMM hh:mm:ss a">
+                  {selectedScreenshot?.capturedAt}
+                </Moment>
+              </div>
+
+              <div className="p-6 ">
+                <img src={selectedScreenshot?.url} className="mb-4 max-w-lg" />
+              </div>
+
+              <div className="p-4 flex space-x-4">
+                <Button>Capture Now</Button>
+                <Button variant="filled" color="warning">
+                  Delete
+                </Button>
+              </div>
+            </div>
+
+            <div className="w-2/5 max-h-[70vh] overflow-auto">
+              <div className="text-xl mb-4">Timeline</div>
+              {response?.data?.map((screenshot) => (
+                <ScreenshotItem
+                  key={screenshot.id}
+                  screenshot={screenshot}
+                  selected={screenshot.id === selectedScreenshot?.id}
+                  onSelect={(screenshot) => setSelectedScreenshot(screenshot)}
+                />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="flex grow flex-col items-center justify-center mt-48 h-[50vh]">
+            <RiErrorWarningLine size={48} className="text-gray-500" />
+            <p className="mt-4 text-center text-gray-500">
+              No screenshots found
+            </p>
+          </div>
+        ))}
     </div>
   );
 }
