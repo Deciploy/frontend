7393bf597efb2899035875a58dd3de5b8bbb17bc
imasshaWe <imasha98.we@gmail.com>
2024-06-14 21:10:33 +0530

    refactor: Update useTrackingDataFetch function to remove unnecessary query parameter

--- src/api/insights.ts
+++ src/api/insights.ts
@@ -1,4 +1,4 @@
 import { Activity, NetworkResponse } from "@types";
 import { useFetch } from "src/app/utils/hooks";
 
-export const useTrackingDataFetch = (prams: string) => useFetch<NetworkResponse<Array<Activity>>>(`tracking/activity?${prams}`);
+export const useTrackingDataFetch = (prams: string) => useFetch<NetworkResponse<Array<Activity>>>(`tracking/activity${prams}`);
