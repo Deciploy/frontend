44805c543f6dfaf1c3cb501ac511dfe970c4ddb7
imasshaWe <imasha98.we@gmail.com>
2024-08-09 20:32:53 +0530

    feat: Add helper functions for calculating employee work time and status

---
+++ src/app/utils/helpers/work-time.ts
@@ -0,0 +1,19 @@
+import { WorkTime } from '@types';
+
+export const getEmployeeAverageWorkTime = (workTimes: Array<WorkTime>) => {
+  if (workTimes.length === 0) {
+    return 0;
+  }
+
+  return workTimes.reduce((a, b) => a + b.time, 0) / (3600 * workTimes.length);
+};
+
+export const getEmployeeWorkStatus = (averageWorkTime: number) => {
+  if (averageWorkTime > 8) {
+    return 'Overtime';
+  } else if (averageWorkTime === 8) {
+    return 'Regular';
+  } else {
+    return 'Underwork';
+  }
+};
