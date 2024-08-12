05b3eed75afd076f6c1f7d2e4fb9750de7698e06
imasshaWe <imasha98.we@gmail.com>
2024-08-12 18:28:12 +0530

    feat: Update home page layout to display CompanyPerformanceChart, EmployeeScoreCard, and TeamScoreCard components

--- src/app/view/pages/home/HomePage.tsx
+++ src/app/view/pages/home/HomePage.tsx
@@ -1,9 +1,20 @@
 import { FC } from 'react';
 
+import CompanyPerformanceChart from './components/CompanyPerformanceChart';
+import EmployeeScoreCard from './components/EmployeeScoreCard';
+import TeamScoreCard from './components/TeamScoreCard';
+
 const HomePage: FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="grid grid-cols-3 gap-2">
+      <div className="col-span-2">
+        <CompanyPerformanceChart />
+      </div>
+
+      <div className="flex flex-col">
+        <EmployeeScoreCard />
+        <TeamScoreCard />
+      </div>
     </div>
   );
 };
